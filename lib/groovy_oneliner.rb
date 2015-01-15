0a503cfcc3d7de08ad27c9ae26adfa913c6204ea
Nasir Jamal <njamal@housetrip.com>
2015-01-15 19:01:16 +0000

    Allow multiple groovy files to be concatenated into a line

--- lib/groovy_oneliner.rb
+++ lib/groovy_oneliner.rb
@@ -11,18 +11,21 @@ class GroovyOneliner
   end
 
   def compute(options = {})
-    path         = options.fetch(:path)
+    paths        = Array(options.fetch(:path))
     should_cache = options.fetch(:cache, false) || self.class.always_cache
 
+    cache_path   = paths.join
+
     if should_cache
-      cached_content = @cache[path]
+      cached_content = @cache[cache_path]
       return cached_content if cached_content
     end
 
-    content = File.read(path)
+    content = paths.reduce([]) { |script, path| script << File.read(path) }.join(';')
+
     output  = GroovyOneliner::Converter.new(content).compute
 
-    should_cache ? @cache[path] = output : output
+    should_cache ? @cache[cache_path] = output : output
   end
 
   def self.compute(options = {})
@@ -38,6 +41,4 @@ class GroovyOneliner
   def self.always_cache
     @@always_cache
   end
-
 end
-
--- spec/groovy_oneliner_spec.rb
+++ spec/groovy_oneliner_spec.rb
@@ -19,21 +19,67 @@ RSpec.describe GroovyOneliner do
       subject.compute(options)
     end
 
+    context 'file contents' do
+      it 'returns the file contents' do
+        allow(File).to receive(:read).with(path).and_return("foo = 1;")
+        expect(subject.compute(options)).to eql('foo=1;')
+      end
+
+      context 'with multiple files' do
+        let(:path) { ['path/to/file', 'path/to/file2'] }
+
+        before do
+          allow(File).to receive(:read).with(path[0]).and_return("foo = 1;")
+          allow(File).to receive(:read).with(path[1]).and_return("bar = 2;")
+        end
+
+        it 'returns contents with ; added in between each file content' do
+          expect(subject.compute(options)).to eql('foo=1;;bar=2;')
+        end
+      end
+    end
+
     context 'when caching is off' do
-      it 'reads the file as many times as calls to compute' do
-        expect(File).to receive(:read).twice.with(path).and_return("foo = 1;")
-        subject.compute(options)
-        subject.compute(options)
+      context 'with single file' do
+        it 'reads the file as many times as calls to compute' do
+          expect(File).to receive(:read).twice.with(path).and_return("foo = 1;")
+          subject.compute(options)
+          subject.compute(options)
+        end
+      end
+
+      context 'with multiple files' do
+        let(:path) { ['path/to/file', 'path/to/file2'] }
+
+        it 'reads both files as many times as calls to compute' do
+          expect(File).to receive(:read).twice.with(path[0]).and_return("foo = 1;")
+          expect(File).to receive(:read).twice.with(path[1]).and_return("foo = 1;")
+          subject.compute(options)
+          subject.compute(options)
+        end
       end
     end
 
     context 'when caching is on' do
       let(:options) { { path: path, cache: true } }
 
-      it 'reads the file only once' do
-        expect(File).to receive(:read).once.with(path).and_return("foo = 1;")
-        subject.compute(options)
-        subject.compute(options)
+      context 'with single file' do
+        it 'reads the file only once' do
+          expect(File).to receive(:read).once.with(path).and_return("foo = 1;")
+          subject.compute(options)
+          subject.compute(options)
+        end
+      end
+
+      context 'with multiple files' do
+        let(:path) { ['path/to/file', 'path/to/file2'] }
+
+        it 'reads both files as many times as calls to compute' do
+          expect(File).to receive(:read).once.with(path[0]).and_return("foo = 1;")
+          expect(File).to receive(:read).once.with(path[1]).and_return("foo = 1;")
+          subject.compute(options)
+          subject.compute(options)
+        end
       end
     end
 
